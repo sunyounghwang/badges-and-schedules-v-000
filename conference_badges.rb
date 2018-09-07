b8f45eb155410018170d1d355c9fb260d2267cac
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-07 23:11:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map {|attendee, i| "Hello, #{attendee}! You'll be assigned to room #{i}!”}
+  attendees.each_with_index.map {|attendee, i| "Hello, #{attendee}! You'll be assigned to room #{i}!"} 
 end
 
 def printer(attendees)
